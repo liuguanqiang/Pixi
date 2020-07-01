1de615f0430974b3a59fa5adb56aab3065979fa5
liuguanqiang <liuguanqiang@hetao101.com>
2020-07-01 19:51:45 +0800

    11

--- script/audioPlay.js
+++
@@ -1,9 +0,0 @@
-function createAudio(url, volume = 1) {
-    let audio = document.createElement("audio");
-    audio.src = url;
-    audio.autoplay = true;
-    audio.controls = false;
-    audio.volume = volume;
-    audio.setAttribute("url", url);
-    return audio;
-}
